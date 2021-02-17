0731efab145969353b304cbc86819623ea9b5a18
Anthony-Rodriguez <7Anthony.Rodriguez7@gmail.com>
2021-02-17 14:56:23 -0500

    Add optional gzip functionality

--- artifacts/settings.py
+++ artifacts/settings.py
@@ -171,6 +171,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 # optional package: http://whitenoise.evans.io/en/stable/django.html
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
